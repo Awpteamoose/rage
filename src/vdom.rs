9fb3c364eddb15d45cb76d99e8905f5facb2c370
Awpteamoose <awpteamoose@gmail.com>
2018-10-16 02:38:55 +0300

    vdom wip

---
+++ src/vdom.rs
@@ -0,0 +1,9 @@
+// node and element traits
+// each node stores a reference to their real dom node
+// diff as I do right now, except return update pairs, to_delete and to_append and then work on real dom
+// ???
+// fast as fukkkkkkkkkkk
+
+// struct Element {
+//     children: Vec<Element>,
+// }
